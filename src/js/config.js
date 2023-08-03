4d3cb8a155e8124a10e0f01dc056c7178cefc05e
HabibaDeghedy <habibaa.deghedy@gmail.com>
2023-08-03 19:34:20 +0300

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'c05a8571-016e-448b-a574-6a21aae6ba85';
+export const MODAL_CLOSE_SEC = 2.5;
